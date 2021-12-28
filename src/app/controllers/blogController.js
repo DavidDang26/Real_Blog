0ad48392321ca2040d61a6147bc49bfb7559d534
thekingincatsle <dat.dt200133@sis.hust.edu.vnm>
2021-12-28 17:19:23 +0700

    overflow

--- src/app/controllers/blogController.js
+++ src/app/controllers/blogController.js
@@ -1,4 +1,5 @@
 const Blog = require("../models/Blog");
+const {multiMongooseToObject} = require('../../util/mongoose')
 class BlogController{
     index(req, res){
         res.render('blogs');
@@ -6,7 +7,7 @@ class BlogController{
     show(req,res){
         Blog.find({}, function (err, blogs) {
             if (!err) {
-              blogs = blogs.map(blog => blog.toObject());
+              blogs = multiMongooseToObject(blogs);
               const currentBlog = blogs.find(blog => blog.test === req.params.test);
               res.render("blog", {blog: currentBlog});
             } else res.status(400).json({ error: "Error!" });
